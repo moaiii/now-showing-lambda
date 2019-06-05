e215d9340c454a08ed92584b817c2147c0f98d5a
chrismelville <chrismelville@skyscanner.net>
2019-06-05 10:53:14 +0100

    updates

--- app.js
+++ app.js
@@ -4,18 +4,15 @@ const request = require('request');
 
 module.exports = api;
 
-api.get('/showings', function (req) {
+api.get('/showings', async function (req) {
 	try {
-		const { APP_ID, API_KEY } = process.env;
+		const { API_ID, API_KEY } = process.env;
 		const { searchedPostcode, endPostCode } = req.queryString;
 		const baseURL = `https://transportapi.com/v3`;
-		const url = `${baseURL}/uk/public/journey/from/postcode:${searchedPostcode}/to/postcode:${endPostCode}.json?app_id=${APP_ID}&app_key=${API_KEY}&service=southeast`
+		const url = `${baseURL}/uk/public/journey/from/postcode:${searchedPostcode}/to/postcode:${endPostCode}.json?app_id=${API_ID}&app_key=${API_KEY}&service=southeast`
 
-		request
-			.get(url)
-			.then((data) => {
-				return data;
-			});
+		const data = await request.get(url)
+		return data;
 
 	} catch (e) {
 		return e;
